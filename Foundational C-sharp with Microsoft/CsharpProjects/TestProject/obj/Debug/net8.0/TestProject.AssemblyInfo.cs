4b1b668cee2737427e7bb310b425734db11888cc
kofiad <kofiadaba@gmail.com>
2024-01-10 23:35:08 +0000

    making progress

--- Foundational C-sharp with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C-sharp with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481c2e75de7782d790db942b8125040d5c35a063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5ab030255b72a979dc6cd061e02705793d0093")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
