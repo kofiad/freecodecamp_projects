27dcc636970dd9dddde41dbb74868f1157131a61
kofiad <kofiadaba@gmail.com>
2024-01-22 02:38:02 +0000

    Create decision logic with if statements

--- Foundational C-sharp with Microsoft/CsharpProjects0/TestProject/Program.cs
+++ Foundational C-sharp with Microsoft/CsharpProjects0/TestProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random num = new Random();
+int num1 = num.Next(1, 7);
+int num2 = num.Next(1, 7);
+int num3 = num.Next(1, 7);
+int sum = num1 + num2 + num3;
+
+Console.WriteLine($"Sum: {num1} + {num2} + {num3} = {sum}");
+if ((num1 == num2) || (num1 == num3) || (num2 == num3))
+{
+    Console.WriteLine("+2 bonus points, doubles");
+    sum += 2;
+}
+if ((num1 == num2) && (num2== num3))
+{
+    Console.WriteLine("+6 bonus points, triples");
+    sum += 6;
+}
+if (sum >= 15)
+{
+    Console.WriteLine("YOU WON");
+}
+if (sum < 15)
+{
+    Console.WriteLine("Sorry, try again");
+}
