c2c833fefce7e6dc982439af5964194e1af6ee5d
kofiad <kofiadaba@gmail.com>
2024-01-22 03:45:01 +0000

    work in progress

---
+++ Foundational C-sharp with Microsoft/OrderOfIDs/TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] id_number = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (var id in id_number)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine(id);
+    }
+}
